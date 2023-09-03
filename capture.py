837c6d5095062dd5a09facf61596f74b35c7391d
Dell <92345385+ApatheticDELL@users.noreply.github.com>
2023-09-03 02:38:04 -0400

    updated capture.py

--- capture.py
+++ capture.py
@@ -8,6 +8,13 @@ import datetime
 import re
 
 os.system('cls' if os.name == 'nt' else 'clear')
+def RmTemp():
+    try:
+        os.remove("XMLprocess/Temp.xml")
+    except:
+        pass
+
+RmTemp
 
 while True:
     TCP_IP = "streaming1.naad-adna.pelmorex.com"
@@ -17,6 +24,7 @@ while True:
     NAAD1.connect((TCP_IP, TCP_PORT))
     print("WE ARE CONNECTED TO PELMOREX NAAD 1")
     print(NAAD1)
+    print("")
     NAAD1.settimeout(90)
     
     try:
@@ -27,12 +35,21 @@ while True:
         time.sleep(3)
         break
     else:
-        #print(str(data))
-        # - to _
-        # + to p
-        # : to _
-        #ONLY IN sent AND ident
-        
+        timeQ = f"XMLprocess/Temp.xml"
+        alert = open(timeQ, 'a')
+        alert.write(str(data))
+
+        while '</alert>' not in data:
+            data = str(NAAD1.recv(BUFFER_SIZE),encoding='utf-8', errors='ignore')
+            alert.write(str(data))
+
+        alert.close()
+
+        file = open(timeQ, "r")
+        data = "".join(line.strip() for line in file.read().splitlines())
+        file.close()
+
+        print("Captured something...")
         try:
             sent = re.search(r"<sent>\s*(.*?)\s*</sent>", str(data), re.MULTILINE | re.IGNORECASE | re.DOTALL).group(1)
             sent = sent.replace("-","_").replace("+", "p").replace(":","_")
@@ -46,25 +63,16 @@ while True:
             print("sent =", str(identifier))
         except:
             print("somthing really brokey :P")
-
-        timeQ = f"XMLprocess/{sent}I{identifier}.xml"
-        timeX = f"XMLqueue/{sent}I{identifier}.xml"
-        print(f"{timeQ}\n{timeX}")
-
-        alert = open(timeQ, 'a')
-        alert.write(str(data))
         
-        while '</alert>' not in data:
-            data = str(NAAD1.recv(BUFFER_SIZE),encoding='utf-8', errors='ignore')
-            #print(str(data))
-            alert.write(str(data))
-                
-        alert.close()
+        timeX = f"XMLqueue/{sent}I{identifier}.xml"
+        print(f"\nSending files...\n{timeX}")
         shutil.move(timeQ, timeX)
         print(time.strftime("Transmission recived at %H:%M:%S on %d/%m/%Y"))
+        print("")
     NAAD1.close()
-    
+
 #This is like the code above, but preformed just incase NAADs 1 fails
+RmTemp
 TCP_IP = "streaming2.naad-adna.pelmorex.com"
 TCP_PORT = 8080
 BUFFER_SIZE = 100000
@@ -82,8 +90,21 @@ except socket.timeout:
     NAAD2.close()
     time.sleep(3)
 else:
-    #print(str(data))
+    timeQ = f"XMLprocess/Temp.xml"
+    alert = open(timeQ, 'a')
+    alert.write(str(data))
 
+    while '</alert>' not in data:
+        data = str(NAAD1.recv(BUFFER_SIZE),encoding='utf-8', errors='ignore')
+        alert.write(str(data))
+
+    alert.close()
+
+    file = open(timeQ, "r")
+    data = "".join(line.strip() for line in file.read().splitlines())
+    file.close()
+
+    print("Captured something...")
     try:
         sent = re.search(r"<sent>\s*(.*?)\s*</sent>", str(data), re.MULTILINE | re.IGNORECASE | re.DOTALL).group(1)
         sent = sent.replace("-","_").replace("+", "p").replace(":","_")
@@ -97,22 +118,12 @@ else:
         print("sent =", str(identifier))
     except:
         print("somthing really brokey :P")
-    
-    timeQ = f"XMLprocess/{sent}I{identifier}.xml"
+        
     timeX = f"XMLqueue/{sent}I{identifier}.xml"
-    print(f"{timeQ}\n{timeX}")
-
-    alert = open(timeQ, 'a')
-    alert.write(str(data))
-      
-    while '</alert>' not in data:
-        data = str(NAAD2.recv(BUFFER_SIZE),encoding='utf-8', errors='ignore')
-        #print(str(data))
-        alert.write(str(data))
-                
-    alert.close()
+    print(f"\nSending files...\n{timeX}")
     shutil.move(timeQ, timeX)
     print(time.strftime("Transmission recived at %H:%M:%S on %d/%m/%Y"))
+    print("")
 NAAD2.close()
 
 #867-5309 :3
